5dc6af3445872157795d8b71e7f2f49c1bcbc2e2
Rayane Souza <110144500+Amannitta@users.noreply.github.com>
2024-04-02 02:36:08 -0300

    questao5.js

--- questao5.js
+++ questao5.js
@@ -1,13 +1,11 @@
 function inverterCaracteres(string) {
     let stringInvertida = "";
-    // Percorre a string de trás para frente e concatena cada caractere à string invertida
     for (let i = string.length - 1; i >= 0; i--) {
         stringInvertida += string[i];
     }
     return stringInvertida;
 }
 
-// Exemplo de uso
 const entrada = "Hello";
 const stringInvertida = inverterCaracteres(entrada);
 console.log("String original:", entrada);
